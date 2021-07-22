3bc8ec47fbf4fe17ad98fdc4255208c1cdabe209
sameraslan <82460915+sameraslan@users.noreply.github.com>
2021-07-22 11:27:25 -0400

    Completed Model and Ran

--- NN Movie Review Classification.py
+++ NN Movie Review Classification.py
@@ -28,9 +28,21 @@ test_data = keras.preprocessing.sequence.pad_sequences(test_data, value=word_ind
 
 # Model Definition
 model = keras.Sequential()
-model.add(keras.layers.Embedding(10000, 16))
-model.add(keras.layers.GlobalAveragePooling1D(10000, 16))
-model.add(keras.layers.Dense(16, activation='relu'))
-model.add(keras.layers.Dense(1, activation='sigmoid'))
+model.add(keras.layers.Embedding(88000, 16))
+model.add(keras.layers.GlobalAveragePooling1D())
+model.add(keras.layers.Dense(16, activation="relu"))
+model.add(keras.layers.Dense(1, activation="sigmoid"))
 
+model.summary()
 
+model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
+
+x_val = train_data[:10000]
+x_train = train_data[10000:]
+
+y_val = train_labels[:10000]
+y_train = train_labels[10000:]
+
+fitModel = model.fit(x_train, y_train, epochs=40, batch_size=512, validation_data=(x_val, y_val), verbose=1)
+results = model.evaluate(test_data, test_labels)
+print(results)

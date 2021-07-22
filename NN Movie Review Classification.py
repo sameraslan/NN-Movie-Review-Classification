923956e05aaa7ac6ebe5f59287937ce55c48d5c4
sameraslan <82460915+sameraslan@users.noreply.github.com>
2021-07-22 12:21:00 -0400

    Saved model and tested on various online reviews

--- NN Movie Review Classification.py
+++ NN Movie Review Classification.py
@@ -4,7 +4,7 @@ import numpy as np
 
 data = keras.datasets.imdb
 
-(train_data, train_labels), (test_data, test_labels) = data.load_data(num_words=10000)
+(train_data, train_labels), (test_data, test_labels) = data.load_data(num_words=88000)
 
 word_index = data.get_word_index()
 word_index = {k:(v+3) for k, v in word_index.items()}
@@ -27,7 +27,7 @@ test_data = keras.preprocessing.sequence.pad_sequences(test_data, value=word_ind
 #print(len(test_data[3]), len(test_data[5]))
 
 # Model Definition
-model = keras.Sequential()
+'''model = keras.Sequential()
 model.add(keras.layers.Embedding(88000, 16))
 model.add(keras.layers.GlobalAveragePooling1D())
 model.add(keras.layers.Dense(16, activation="relu"))
@@ -45,4 +45,38 @@ y_train = train_labels[10000:]
 
 fitModel = model.fit(x_train, y_train, epochs=40, batch_size=512, validation_data=(x_val, y_val), verbose=1)
 results = model.evaluate(test_data, test_labels)
+
 print(results)
+model.save('model.h5')'''
+
+model = keras.models.load_model('model.h5')
+
+def review_encode(s):
+    encoded = [1]
+
+    for word in s:
+        if word.lower() in word_index:
+            encoded.append(word_index[word.lower()])
+        else:
+            encoded.append(2)
+
+    return encoded
+
+with open("reviewpredict.rtf", encoding="utf-8") as f:
+    for line in f.readlines():
+        nline = line.replace(",", "").replace(".", "").replace("(", "").replace(")", "").replace(":", "").replace("\"","").strip().split(" ")
+        encode = review_encode(nline)
+        encode = keras.preprocessing.sequence.pad_sequences([encode], value=word_index["<PAD>"], padding="post", maxlen=250) # make the data 250 words long
+        predict = model.predict(encode)
+        print(line)
+        print(encode)
+        print(predict[0])
+
+'''test_review = test_data[0]
+predict = model.predict([test_review])
+print("Review: " + str(decode_review(test_review)))
+print("Prediction: " + str(predict[0]))
+print("Actual: ", str(test_labels[0]))
+print(results)
+model.save('model.h5')'''
+
